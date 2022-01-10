f51f05d5c11b81a54027eec360ccb4c0e0c5e6bd
SzHHard <korenkov_misha8611111@mail.ru>
2022-01-10 12:42:46 +0300

    added 404page

--- src/pages/ErrorScreens/404Screen/index.js
+++ src/pages/ErrorScreens/404Screen/index.js
@@ -7,8 +7,10 @@ let page404 = ejs.render(screen404, object)
 
 let root = document.querySelector('#root');
 
-let exist = ['/', '/login', '/bug', '/chats', '/registration']
+let exist = ['/', '/currentChat', '/login', '/registration', '/account', '/updateInfo', '/updatePassword', '/bug', ]
+
 
 if(!exist.includes(window.location.pathname)){  
+    console.log(window.location.pathname)
     root.innerHTML = page404;
 }
